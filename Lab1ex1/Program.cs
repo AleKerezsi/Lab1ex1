2daaca7687252ee5ff3f5f9b1c4d32e165466478
alexandra.kerezsi.18@gmail.com <alexandra.kerezsi.18@gmail.com>
2022-11-27 15:34:57 +0200

    lab1ex1

---
+++ Lab1ex1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lab1ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Scrieti un program care citind de la tastaura cele trei dimensiuni ale unui
+               paralelipiped dreptunghic, va afisa volumul lui */
+
+            Console.WriteLine("Introduceti latura 1 a paralelipipedului dreptunghic: ");
+            int latura1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti latura 2 a paralelipipedului dreptunghic: ");
+            int latura2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti latura 3 a paralelipipedului dreptunghic: ");
+            int latura3 = int.Parse(Console.ReadLine());
+
+            int volum = latura1 * latura2 * latura3;
+            Console.WriteLine("Volumul paralelipipedului dreptunghic este: " + volum);
+
+
+        }
+    }
+}
